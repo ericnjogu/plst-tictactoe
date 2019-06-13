a83ae7a29e21ea97c1607ce8c39096b48130b250
Eric Njogu <mugo@Erics-MacBook-Pro.local>
2019-06-13 10:24:58 +0300

    fix: adding docstrings suggested by pylint-django

--- player/urls.py
+++ player/urls.py
@@ -1,3 +1,4 @@
+""" url patterns"""
 from django.conf.urls import re_path
 from .views import home
 
--- player/views.py
+++ player/views.py
@@ -1,7 +1,9 @@
+"""controller logic for player app"""
 from django.shortcuts import render
 from gameplay.models import Game
 # Create your views here.
 
 def home(request):
+    """respond to /home requests"""
     return render(request, "player/home.html",
         {'ngames': Game.objects.count()})
