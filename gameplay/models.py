c03b0e273332583ed937e594ee7e94c916e92326
Eric Njogu <mugo@Erics-MacBook-Pro.local>
2019-06-14 10:23:22 +0300

    fix: applying pylint suggestions

--- gameplay/models.py
+++ gameplay/models.py
@@ -16,15 +16,17 @@ class GameQuerySet(QuerySet):
     def games_for_user(self, user):
         """all games where the user is first or second player"""
         return self.filter(Q(first_player=user) | Q(second_player=user))
-    
+
     def active(self):
         """all active games"""
         return self.filter(Q(status='F') | Q(status='S'))
 
 class Game(models.Model):
     """a tictactoe Game"""
-    first_player = models.ForeignKey(User, related_name='games_first_player',on_delete=models.CASCADE)
-    second_player = models.ForeignKey(User, related_name='games_second_player', on_delete=models.CASCADE)
+    first_player = models.ForeignKey(User, related_name='games_first_player',
+                                     on_delete=models.CASCADE)
+    second_player = models.ForeignKey(User, related_name='games_second_player',
+                                      on_delete=models.CASCADE)
     start_time = models.DateTimeField(auto_now_add=True)
     end_time = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=1, default='F', choices=GAME_STATUS_CHOICES)
