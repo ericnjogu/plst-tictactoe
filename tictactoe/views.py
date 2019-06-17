752338e610db7835c9e21a69bfddbfa849ada1f5
Eric Njogu <mugo@Erics-MacBook-Pro.local>
2019-06-17 14:01:40 +0300

    fix: adding doc strings and complying with pylint

--- tictactoe/views.py
+++ tictactoe/views.py
@@ -1,4 +1,6 @@
+""" controller logic for the default app """
 from django.shortcuts import render
 
 def welcome(request):
+    """ show a welcome page """
     return render(request, "tictactoe/welcome.html")
