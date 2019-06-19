9478310cbc3516ec9e07c275afd40e64febc45ba
Eric Njogu <mugo@Erics-MacBook-Pro.local>
2019-06-19 16:04:23 +0300

    feat: handling, validating 'invite' submission

--- player/views.py
+++ player/views.py
@@ -1,9 +1,10 @@
 """controller logic for player app"""
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 from gameplay.models import Game
 from .forms import InvitationForm
+from player.models import Invitation
 
 # Create your views here.
 
@@ -19,6 +20,13 @@ def home(request):
 @login_required
 def new_invitation(request):
     """ show an a invitation form """
-    form = InvitationForm()
+    if request.method == 'POST':
+        invite = Invitation(from_user=request.user)
+        form = InvitationForm(instance=invite, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('player_home')
+    else:
+        form = InvitationForm()
     return render(request, "player/new_invitation_form.html", {'form': form})
     
