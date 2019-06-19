efae2caee64d5a419151e70a712ef8eacc56149f
Eric Njogu <mugo@Erics-MacBook-Pro.local>
2019-06-19 12:37:28 +0300

    feat: adding model 'Invitation' to 'player' app

--- player/models.py
+++ player/models.py
@@ -1,3 +1,11 @@
+""" app 'player' models """
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Invitation(models.Model):
+    """ an invitation to play a tictactoe game """
+    from_user = models.ForeignKey(User, related_name='invitations_sent', on_delete=models.CASCADE)
+    to_user = models.ForeignKey(User, related_name='invitations_received', on_delete=models.CASCADE)
+    message = models.CharField(max_length=300)
+    timestamp = models.DateTimeField(auto_now_add=True)
