848887a9eea267eb3c85740b6f15d24d2f1fe17a
Eric Njogu <mugo@Erics-MacBook-Pro.local>
2019-06-20 13:41:44 +0300

    feat: adding info messages for use on the form

--- player/models.py
+++ player/models.py
@@ -6,6 +6,15 @@ from django.contrib.auth.models import User
 class Invitation(models.Model):
     """ an invitation to play a tictactoe game """
     from_user = models.ForeignKey(User, related_name='invitations_sent', on_delete=models.CASCADE)
-    to_user = models.ForeignKey(User, related_name='invitations_received', on_delete=models.CASCADE)
-    message = models.CharField(max_length=300)
+    to_user = models.ForeignKey(
+        User,
+        related_name='invitations_received',
+        on_delete=models.CASCADE,
+        verbose_name="User to invite",
+        help_text="select from list")
+    message = models.CharField(
+        max_length=300,
+        verbose_name='optional message',
+        help_text='personal invitation',
+        blank=True)
     timestamp = models.DateTimeField(auto_now_add=True)
