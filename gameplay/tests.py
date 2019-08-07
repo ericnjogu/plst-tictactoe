aa8ef139af8b61f24c03ed5491da267e464835ab
Eric Njogu <kunadawa@gmail.com>
2019-08-07 16:14:20 +0000

    test: new move should not be None

--- gameplay/tests.py
+++ gameplay/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Game
+from django.test import TestCase
+
+class GameTest(TestCase):
+    def test_new_move_is_not_null(self):
+        game = Game()
+        new_move = game.new_move()
+        self.assertNotEqual(new_move, None)
 
 # Create your tests here.
